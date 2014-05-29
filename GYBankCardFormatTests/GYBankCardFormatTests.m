123c2f888b45904da16c877ab8f5c9e41485192c
Gary <p709723778@126.com>
2014-05-29 17:14:29 +0800

    Initial Commit

---
+++ GYBankCardFormatTests/GYBankCardFormatTests.m
@@ -0,0 +1,34 @@
+//
+//  GYBankCardFormatTests.m
+//  GYBankCardFormatTests
+//
+//  Created by Gary on 14-5-29.
+//  Copyright (c) 2014年 蒲晓涛. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GYBankCardFormatTests : XCTestCase
+
+@end
+
+@implementation GYBankCardFormatTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
